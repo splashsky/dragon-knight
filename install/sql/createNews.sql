449ba8df351f8c7ac673c4183bba888706f8c9e8
Splashsky <skylearj@gmail.com>
2021-01-29 11:29:01 -0600

    Removing all old code to replace with laravel framework.

--- install/sql/createNews.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS {{ table }};
-
-CREATE TABLE {{ table }} (
-    id int unsigned NOT NULL auto_increment,
-    user_id int unsigned,
-    posted datetime,
-    content text NOT NULL,
-    PRIMARY KEY (id)
-);
--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'app/Game.php';
-require 'app/Models/Fight.php';
-
-$db = new Database(config('game.db'));
-$fight = new Fight($db, 1);
-dd($fight);
